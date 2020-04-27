82c0c5275f66dfb7295f171a4670879354615665
ravi-panara <rravi.panara@gmail.com>
2020-04-27 23:14:56 +0530

    Updates

---
+++ precache-manifest.5a340d6742cfd787a3684f1a937ab714.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "711a153a40ede4538d0a18c1d6b26ad5",
+    "url": "/avatar-kingdom/index.html"
+  },
+  {
+    "revision": "5d72349a8d8309d5edb2",
+    "url": "/avatar-kingdom/static/css/main.12c0ba57.chunk.css"
+  },
+  {
+    "revision": "32fd67051b2a490fb47c",
+    "url": "/avatar-kingdom/static/js/2.212a7e2c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/avatar-kingdom/static/js/2.212a7e2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d72349a8d8309d5edb2",
+    "url": "/avatar-kingdom/static/js/main.dcf7ad54.chunk.js"
+  },
+  {
+    "revision": "bc4e8b811022719f5402",
+    "url": "/avatar-kingdom/static/js/runtime-main.11dea36b.js"
+  }
+]);
